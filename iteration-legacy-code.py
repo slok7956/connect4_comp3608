32d66e220eb6e7360f060a21f855965d88a45745
slok7956 <129351126+slok7956@users.noreply.github.com>
2023-04-02 12:43:20 +1000

    Create iteration-legacy-code.py

---
+++ iteration-legacy-code.py
@@ -0,0 +1,19 @@
+    #expand next level of nodes
+    i = 0
+    while i < 7:
+        #set child board state
+        next_state, column = change_state(state, turn, i)
+        #i = column
+        #calculate score
+        next_score=evaluation(next_state)
+        #alpha,beta=a_b_update(next_turn,next_score,alpha,beta)
+        #check for pruning
+        to_prune=prune(next_turn,next_score,alpha,beta)
+        if to_prune:
+            return score
+        else:  
+          #expand next node
+          fin_dict.update({i:recurse(next_state, next_turn, next_score, alpha, beta, depth-1)})
+        #advance column
+        #i = column
+        i += 1
